f35266644a2f58a7d7f22d2ab4cf47dcb4950ccb
Sebastian Porto <s@porto5.com>
2019-12-02 20:51:02 +1100

    Solve part 2

--- 01/main.nim
+++ 01/main.nim
@@ -1,4 +1,4 @@
-import math, strutils
+import math, strutils, sugar
 import sequtils
 
 proc read_file(): seq[int] =
@@ -7,20 +7,23 @@ proc read_file(): seq[int] =
     file.splitLines().map(parseInt)
 
 
-proc fuel_for_module(mass: int): float =
+proc fuel_for_module(mass: float): float =
     (mass / 3).floor - 2
+
+proc total_fuel_for_module(acc: float, mass: float): float =
+    let res = mass.fuel_for_module
+    if res > 0:
+        total_fuel_for_module(acc + res, res)
+    else:
+        acc
 
 proc calc(input: seq[int]): float =
     input
-        .map(fuel_for_module)
+        .map(v => total_fuel_for_module(0, v.to_float))
         .foldl(a + b)
 
 proc main(): float =
     let input = read_file()
     calc(input)
-
-
-
-# echo fuel_for_module(100756)
 
 echo main()

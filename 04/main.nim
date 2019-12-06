e9498322a1a5d723e8ac5c466aebc027289e4df9
Sebastian Porto <s@porto5.com>
2019-12-06 19:45:51 +1100

    Add hasTwo

--- 04/main.nim
+++ 04/main.nim
@@ -1,18 +1,34 @@
-import sequtils, math
+import sequtils, math, strutils, sugar
 
 const R = 367479 .. 893698
+
+proc print_return[T](v: T): T =
+    echo v
+    v
 
 proc digits(n: int): seq[int] =
     for ix in countdown(5, 0):
         let d = (floor (n.toFloat / pow(10, ix.toFloat))) mod 10
         result.add(d.toInt)
 
-proc windows(s: seq[int]): seq[(int, int)] =
+proc windows[T](s: seq[T]): seq[(T, T)] =
     zip(s[0 .. s.len - 2], s[1 .. s.len - 1])
 
 proc hasTwo(s: seq[int]): bool =
-    s.windows.anyIt(it[0] == it[1])
-
+    let first = s[0 .. 0]
+    concat(first, s)
+        .map(i => intToStr(i))
+        .windows
+        .map(proc (it: tuple[a: string, b: string]): string =
+            if it[0] == it[1]:
+                $(it[1])
+            else:
+                "/" & $(it[1])
+        )
+        .join()
+        .split("/")
+        .map(print_return)
+        .anyIt(it.len == 2)
 
 proc noDecreasing(s: seq[int]): bool =
     s.windows.allIt(it[0] <= it[1])
@@ -29,4 +45,6 @@ proc main(): int =
         .map(isMatch)
         .foldl(a + b)
 
-echo main()
+# echo main()
+
+echo hasTwo(@[2, 1, 3, 3, 1])

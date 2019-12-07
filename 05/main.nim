fe9cc715f8cff6012ef1ea23092e38f1e59386aa
Sebastian Porto <s@porto5.com>
2019-12-07 20:53:20 +1100

    Attempt part b

--- 05/main.nim
+++ 05/main.nim
@@ -7,10 +7,16 @@ type OpCode = enum
     Mul,
     Input,
     Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
     Halt
 
+const NUM_INPUT = 8
+
 proc read_file(): seq[int] =
-    let filename = "input.txt"
+    let filename = "input.2.txt"
     let file = readFile(filename)
     file.split(",").map(parseInt)
 
@@ -24,6 +30,14 @@ proc get_op_code(op: int): OpCode =
             Input
         of 4:
             Output
+        of 5:
+            JumpIfTrue
+        of 6:
+            JumpIfFalse
+        of 7:
+            LessThan
+        of 8:
+            Equals
         of 99:
             Halt
         else:
@@ -49,7 +63,8 @@ proc get_input(memory: seq[int], instruction_pointer: int, op: int,
 
 proc consume(memory: var seq[int], instruction_pointer: int): seq[int] =
     let op = memory[instruction_pointer]
-    # echo op
+    echo instruction_pointer
+    echo op.get_op_code
 
     case op.get_op_code:
         of Add:
@@ -66,20 +81,60 @@ proc consume(memory: var seq[int], instruction_pointer: int): seq[int] =
             memory.consume(instruction_pointer + 4)
         of Input:
             let address = memory[instruction_pointer + 1]
-            memory[address] = 1
+            echo " - input into address " & $(address)
+            memory[address] = NUM_INPUT
             memory.consume(instruction_pointer + 2)
         of Output:
-            let address = memory[instruction_pointer + 1]
-            echo memory[address]
+            let in1 = get_input(memory, instruction_pointer, op, 1)
+            echo " - out " & $(in1)
             memory.consume(instruction_pointer + 2)
+        of JumpIfTrue:
+            let in1 = get_input(memory, instruction_pointer, op, 1)
+            let in2 = get_input(memory, instruction_pointer, op, 2)
+            if in1 == 0:
+                memory.consume(instruction_pointer + 3)
+            else:
+                echo " - jumping to " & $(in2)
+                memory.consume(in2)
+        of JumpIfFalse:
+            let in1 = get_input(memory, instruction_pointer, op, 1)
+            let in2 = get_input(memory, instruction_pointer, op, 2)
+            if in1 == 0:
+                echo " - jumping to " & $(in2)
+                memory.consume(in2)
+            else:
+                memory.consume(instruction_pointer + 3)
+        of LessThan:
+            let in1 = get_input(memory, instruction_pointer, op, 1)
+            let in2 = get_input(memory, instruction_pointer, op, 2)
+            let in3 = get_input(memory, instruction_pointer, op, 3)
+            if in1 < in2:
+                echo " - set " & $(in3) & " to 1"
+                memory[in3] = 1
+            else:
+                echo " - set " & $(in3) & " to 0"
+                memory[in3] = 0
+            memory.consume(instruction_pointer + 4)
+        of Equals:
+            let in1 = get_input(memory, instruction_pointer, op, 1)
+            let in2 = get_input(memory, instruction_pointer, op, 2)
+            let in3 = get_input(memory, instruction_pointer, op, 3)
+            echo " - p1 " & $(in1)
+            echo " - p2 " & $(in2)
+            if in1 == in2:
+                echo " - set " & $(in3) & " to 1"
+                memory[in3] = 1
+            else:
+                echo " - set " & $(in3) & " to 0"
+                memory[in3] = 0
+            memory.consume(instruction_pointer + 4)
         of Halt:
             memory
 
-proc main(): int =
+proc main(): void =
     var memory = read_file()
     let _ = memory.consume(0)
-    memory[0]
 
-echo main()
+main()
 
 # 9025675

b63688ef5e7cda3532a87e8864cc381c37b2574c
Sebastian Porto <s@porto5.com>
2019-12-13 22:58:24 +1100

    Solve 08 part 2

--- 08/main.nim
+++ 08/main.nim
@@ -1,4 +1,4 @@
-import unicode, strutils, sequtils, tables
+import unicode, strutils, sequtils, tables, sugar
 
 const WIDE = 25
 const TALL = 6
@@ -14,14 +14,15 @@ proc read_file(): seq[int] =
         .map(parseInt)
 
 proc count_digits(layer: seq[int]): CountTable[int] =
-    # var table = initCountTable[int]()
     layer.toCountTable
 
-
-proc main(): void =
+proc get_layers(): seq[seq[int]] =
     let all = read_file()
     let layer_count = all.len / DIGITS_IN_LAYER
-    let layers = all.distribute(layer_count.to_int)
+    all.distribute(layer_count.to_int)
+
+proc main_1(): void =
+    let layers = get_layers()
     let counts = layers.map(count_digits)
     var fewest = counts[0]
     for c in counts:
@@ -30,4 +31,33 @@ proc main(): void =
     echo fewest
     echo fewest[1] * fewest[2]
 
-main()
+proc flatten(layers: seq[seq[int]], image: seq[int]): seq[int] =
+    var new_image = newSeq[int](image.len)
+    if layers.len == 0:
+        image
+    else:
+        let layer = layers[0]
+        for ix, _ in layer:
+            if image[ix] == 2: # transparent
+                new_image[ix] = layer[ix]
+            else:
+                new_image[ix] = image[ix]
+        flatten(layers[1 .. layers.len - 1], new_image)
+
+proc print_line(line: seq[int]) : void=
+    var res = ""
+    for c in line:
+        if c == 0:
+            res.add(" ")
+        else:
+            res.add("▮")
+    echo res
+
+proc main_2(): void =
+    let layers = get_layers()
+    let image = layers.flatten(layers[0])
+    let lines = image.distribute(TALL)
+    for l in lines:
+        l.print_line
+
+main_2()

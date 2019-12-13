886ff9ae7822e3134ce3f94658d26473ad028664
Sebastian Porto <s@porto5.com>
2019-12-13 22:23:02 +1100

    Solve 08 part 1

--- 08/main.nim
+++ 08/main.nim
@@ -1,7 +1,8 @@
-import unicode, strutils, sequtils
+import unicode, strutils, sequtils, tables
 
 const WIDE = 25
 const TALL = 6
+const DIGITS_IN_LAYER = WIDE * TALL
 
 const FILENAME = "input.txt"
 
@@ -12,4 +13,21 @@ proc read_file(): seq[int] =
         .map(toUTF8)
         .map(parseInt)
 
-echo read_file()
+proc count_digits(layer: seq[int]): CountTable[int] =
+    # var table = initCountTable[int]()
+    layer.toCountTable
+
+
+proc main(): void =
+    let all = read_file()
+    let layer_count = all.len / DIGITS_IN_LAYER
+    let layers = all.distribute(layer_count.to_int)
+    let counts = layers.map(count_digits)
+    var fewest = counts[0]
+    for c in counts:
+        if c[0] < fewest[0]:
+            fewest = c
+    echo fewest
+    echo fewest[1] * fewest[2]
+
+main()
